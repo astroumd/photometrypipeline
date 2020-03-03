59c048e8195271635d31f5c5ec0bc6b08b2d1bbb
durba <durbak.3@gmail.com>
2020-03-02 22:26:44 -0500

    debugging shutil move

--- test.py
+++ test.py
@@ -70,7 +70,9 @@ print('start move files master biases to selected folder')
 for f in glob.glob(os.path.join(base_path, 'bias*.fits')):
     print('selected_path', selected_path)
     f_base_path = os.path.join(base_path, f)
+    print('f_base_path', f_base_path)
     f_selected_path = os.path.join(selected_path, f)
+    print('f_selected_path', f_selected_path)
     print('moving {0} to {1}'.format(f_base_path, f_selected_path))
     move(f_base_path, f_selected_path)
 

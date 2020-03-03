d267f81da607214e0ed7a44806b1a5c513bdba49
durba <durbak.3@gmail.com>
2020-03-02 22:21:40 -0500

    debugging shutil move

--- test.py
+++ test.py
@@ -66,17 +66,18 @@ preproc.mkmaster('lmi', bias_calib, 'bias')
 print('start mkmaster flat')
 preproc.mkmaster('lmi', flat_calib, 'flat')
 
-print('start copyfiles master biases and flats to selected folder')
+print('start move files master biases to selected folder')
 for f in glob.glob(os.path.join(base_path, 'bias*.fits')):
     f_base_path = os.path.join(base_path, f)
     f_selected_path = os.path.join(selected_path, f)
-    print('moving {} to {}'.format(f_base_path, f_selected_path))
+    print('moving {0} to {1}'.format(f_base_path, f_selected_path))
     move(f_base_path, f_selected_path)
 
+print('start move files master flats to selected folder')
 for f in glob.glob(os.path.join(base_path, 'flat*.fits')):
     f_base_path = os.path.join(base_path, f)
     f_selected_path = os.path.join(selected_path, f)
-    print('moving {} to {}'.format(f_base_path, f_selected_path))
+    print('moving {0} to {1}'.format(f_base_path, f_selected_path))
     move(f_base_path, f_selected_path)
 
 autoproc(datadir=selected_path+os.path.sep,
